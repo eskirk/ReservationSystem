86f827cafb1782574b54f47a004b2a0a4b04922a
Elliot Kirk <elliot.s.kirk@gmail.com>
2018-03-12 17:17:52 -0700

    reformat ResultSet into 2d array, future proofing and whatnot

--- InnReservations.java
+++ InnReservations.java
@@ -6,6 +6,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.LinkedHashMap;
 import java.time.LocalDate;
@@ -16,43 +18,75 @@ import java.time.LocalDate;
 
  pre-compilation stuff:
  export CLASSPATH=$CLASSPATH:mysql-connector-java-5.1.45-bin.jar:.
- export APP_JDBC_URL=<URL>
- export APP_JDBC_USER=<USER>
- export APP_JDBC_PW=<PASS>
+ export APP_JDBC_URL=jdbc:mysql://csc365winter2018.webredirect.org/alam38?autoReconnect=true
+ export APP_JDBC_USER=alam38
+ export APP_JDBC_PW=365W18_010270594
  */
 
 public class InnReservations {
-   public static void main(String[] args) throws SQLException{
-      Connection dbConnection = establishConnection();
-
-      if (dbConnection != null) {
-         System.out.println("Database connection acquired - processing query");
-         // follow through with other query related shenanigans  
-         
-      }
-      else 
-         System.out.println("Database connection cannot be acquired - exiting");
+   public static void main(String[] args) throws SQLException {
+      Connection conn = startProgram();
+      executeQuery(conn);
    }
 
-   public static Connection establishConnection() throws SQLException {
+   /*
+      Acquires and returns a connection based on our environment variables
+   */
+   public static Connection startProgram() throws SQLException {
       String url = System.getenv("APP_JDBC_URL");
       String user = System.getenv("APP_JDBC_USER");
       String pass = System.getenv("APP_JDBC_PW");
-      Connection conn = null;
 
-      try (Connection connection = DriverManager.getConnection(url, user, pass)) {
-         System.out.println("Established connection");
-         conn = connection;
-      }
-      catch (SQLException e) {
-         System.out.println("An exception occured while establishing a connection");
-         System.out.println(e);
-      }
-      return conn;
+      Connection connection = DriverManager.getConnection(url, user, pass);
+      System.out.println("Database connection acquired - processing query");
+      return connection;
    }
 
-   // Executes the given query using the given database connection
-   public static executeQuery(String query, Connection conn) {
+   /*
+      Executes a query and returns a List<String[]> representing the result of
+      the query
+   */
+   public static List<String[]> executeQuery(Connection connection) throws SQLException {
+      String query = "SELECT Room, COUNT(Room) / 180 FROM lab6_reservations WHERE Checkout >= (CURDATE() - INTERVAL 180 DAY) GROUP BY Room ORDER BY COUNT(Room) DESC";
+      Statement stmt = connection.createStatement();
+      ResultSet res = stmt.executeQuery(query);
 
+      // print out the columns
+      try {
+         while (res.next()) {
+            return resultToArray(res);
+         }
+      } catch (SQLException e) {
+         System.out.println("An exception occured");
+         System.out.println(e);
+      }
+      return null;
+   }
+
+   /*
+      Returns a List<String[]> representing the query results
+   */
+   public static List<String[]> resultToArray(ResultSet result) throws SQLException {
+      int cols = result.getMetaData().getColumnCount();
+      List<String[]> table = new ArrayList<>();
+
+      do {
+         String[] row = new String[cols];
+         for (int col = 1; col <= cols; col++) {
+            Object obj = result.getObject(col);
+            row[col - 1] = (obj == null) ? null : obj.toString();
+         }
+         table.add(row);
+      } while(result.next());
+
+      // print result
+      for (String[] row : table) {
+         for (String s : row) {
+            System.out.print(" " + s);
+         }
+         System.out.println();
+      }
+
+      return table;
    }
 }
